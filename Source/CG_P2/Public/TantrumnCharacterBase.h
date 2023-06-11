ec2b8145f247ce23055998ed0618e91b77f046fe
JPK <toximail2@gmail.com>
2023-06-11 17:36:36 -0400

    Throwable object WIP
    * Following code/steps from throwable object section
    * Does not work ATM

--- Source/CG_P2/Public/TantrumnCharacterBase.h
+++ Source/CG_P2/Public/TantrumnCharacterBase.h
@@ -4,7 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "ThrowableActor.h"
+#include "Sound/SoundCue.h"
 #include "TantrumnCharacterBase.generated.h"
+
+UENUM(BlueprintType)
+enum class ECharacterThrowState : uint8
+{
+	None				UMETA(DIsplayName = "None"),
+	RequestingPull		UMETA(DIsplayName = "RequestingPull"),
+	Pulling				UMETA(DIsplayName = "Pulling"),
+	Attached			UMETA(DIsplayName = "Attached"),
+	Throwing			UMETA(DIsplayName = "Throwing"),
+};
+
 
 UCLASS()
 class CG_P2_API ATantrumnCharacterBase : public ACharacter
@@ -15,16 +28,6 @@ public:
 	// Sets default values for this character's properties
 	ATantrumnCharacterBase();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-	UPROPERTY(EditAnywhere, Category = "Fall Impact")
-	float MinImpactSpeed = 800.0f;
-	UPROPERTY(EditAnywhere, Category = "Fall Impact")
-	float MaxImpactSpeed = 1600.0f;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
@@ -33,4 +36,100 @@ public:
 
 	virtual void Landed(const FHitResult& Hit) override;
 
+	UFUNCTION(BlueprintCallable)
+	void RequestThrowObject();
+	UFUNCTION(BlueprintCallable)
+	void RequestPullObjectStart();
+	UFUNCTION(BlueprintCallable)
+	void RequestPullObjectStop();
+	UFUNCTION(BlueprintCallable)
+	void ResetThrowableObject();
+
+	void OnThrowableAttached(AThrowableActor* InThrowableActor);
+
+	void SphereCastPlayerView();
+
+	void SphereCastActorTransform();
+
+	void LineCastActorTransform();
+
+	void ProcessTraceResult(const FHitResult& HitResult);
+
+	bool CanThrowObject() const { return CharacterThrowState == ECharacterThrowState::Attached; }
+
+	UFUNCTION(BlueprintPure)
+		bool IsPullingObject() const { return CharacterThrowState == ECharacterThrowState::RequestingPull || CharacterThrowState == ECharacterThrowState::Pulling; }
+
+	UFUNCTION(BlueprintPure)
+		ECharacterThrowState GetCharacterThrowState() const { return CharacterThrowState; }
+
+	UFUNCTION(BlueprintPure)
+		bool IsStunned() const { return bIsStunned; }
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	bool PlayThrowMontage();
+
+	void UnbindMontage();
+
+	UFUNCTION()
+		void OnMontageBlendingOut(UAnimMontage* Montage, bool bInterrupted);
+
+	UFUNCTION()
+		void OnMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+
+	UFUNCTION()
+		void OnNotifyBeginRecieved(FName NotifyName, const FBranchingPointNotifyPayload& BranchingPointNotifyPayload);
+
+	UFUNCTION()
+		void OnNotifyEndRecieved(FName NotifyName, const FBranchingPointNotifyPayload& BranchingPointNotifyPayload);
+
+	void OnStunBegin(float StunRatio);
+	void UpdateStun();
+	void OnStunEnd();
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		float SprintSpeed = 1200.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Fall Impact")
+		float MinImpactSpeed = 600.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Fall Impact")
+		float MaxImpactSpeed = 1200.0f;
+
+	// Time in Seconds
+	UPROPERTY(EditAnywhere, Category = "Fall Impact")
+		float MinStunTime = 1.0f;
+	// Time in Seconds
+	UPROPERTY(EditAnywhere, Category = "Fall Impact")
+		float MaxStunTime = 1.0f;
+
+	USoundCue* HeavyLandSound = nullptr;
+
+	float StunTime = 0.0f;
+	float StunBeginTimestamp = 0.0f;
+
+	bool bIsStunned = false;
+	bool bIsSprinting = false;
+
+	float MaxWalkSpeed = 0.0f;
+
+	UPROPERTY(VisibleAnywhere, Category = "Throw")
+		ECharacterThrowState CharacterThrowState = ECharacterThrowState::None;
+
+	UPROPERTY(EditAnywhere, Category = "Throw", meta = (ClampMin = "0.0", Unit = "ms"))
+		float ThrowSpeed = 2000.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+		UAnimMontage* ThrowMontage = nullptr;
+
+	FOnMontageBlendingOutStarted BlendingOutDelegate;
+	FOnMontageEnded MontageEndedDelegate;
+
+private:
+
+	UPROPERTY()
+		AThrowableActor* ThrowableActor;
 };
